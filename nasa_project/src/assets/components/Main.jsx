f923fe4c525f4399563e863e7cdff22f6eb159c7
MakiniFab <makinifabean@gmail.com>
2024-07-07 20:24:11 +0300

    components_without_css

---
+++ nasa_project/src/assets/components/Main.jsx
@@ -0,0 +1,11 @@
+function Main(props) {
+    const {data} = props
+
+    return (
+        <div  className="imgContainer">
+            <p>image space</p>
+        </div>
+    )
+}
+
+export default Main;
